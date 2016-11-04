f21c1a92192f9c44d14bd959da22f228928c69da
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-11-04 14:03:54 +0200

    Added e08

---
+++ Unity Projects/E08 Character Controller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 2;
+	public float runSpeed = 6;
+
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+
+	Animator animator;
+	Transform cameraT;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+		cameraT = Camera.main.transform;
+	}
+
+	void Update () {
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 inputDir = input.normalized;
+
+		if (inputDir != Vector2.zero) {
+			float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+		}
+
+		bool running = Input.GetKey (KeyCode.LeftShift);
+		float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+		currentSpeed = Mathf.SmoothDamp (currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+		transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+		float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+		animator.SetFloat ("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+	}
+}
---
+++ Unity Projects/E08 Character Controller/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public bool lockCursor;
+	public float mouseSensitivity = 10;
+	public Transform target;
+	public float dstFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2 (-40, 85);
+
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+
+	float yaw;
+	float pitch;
+
+	void Start() {
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+	}
+
+	void LateUpdate () {
+		yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
+		pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp (currentRotation, new Vector3 (pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+		transform.position = target.position - transform.forward * dstFromTarget;
+
+	}
+
+}
